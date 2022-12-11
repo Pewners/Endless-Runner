c9fa18b0d7df82ba1048b6f4b3d1f62276a798a1
Pewners <jfadikpe2021@elam.co.uk>
2022-12-11 23:16:22 +0000

    added star counter

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    //public int score;
+
+    void Start()
+    {
+        
+    }
+
+   void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreScript : MonoBehaviour{
+public class ScoreScript : MonoBehaviour
+{
 	public Transform player;
 	public Text scoreText;
+	public int score;
+
+    void Start() 
+    {
+		
+    }
 	
-	void Update () {
-		scoreText.text = player.position.x.ToString("0");
+	void Update () 
+	{
+		scoreText.text = score.ToString();
 	}
 }
